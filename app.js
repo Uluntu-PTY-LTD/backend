e8d12fa4477ebd374af7db63c2ad80d8b67292e6
mindsgn <zookid@protonmail.com>
2024-10-19 18:19:07 +0200

    update: remove node_modules

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ const methodOverride = require('method-override')
 const PoolFund = require('./models/pool_funds');
 const User = require('./models/user');
 const Transaction = require('./models/transaction')
-const PORT = 3000; 
+const PORT = process.env.PORT || 3000;
 
 const app = express();
 
@@ -20,13 +20,15 @@ app.set('views', path.join(__dirname, 'views'));
 app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride('_method'));
 
-mongoose.connect('mongodb://localhost:27017/stokvel', {
+mongoose.connect(`${process.env.MONGO_DB_URL}`, {
     useNewUrlParser: true,
     useUnifiedTopology: true
 });
 
 const db = mongoose.connection;
+
 db.on("error", console.error.bind(console, "connection error:"));
+
 db.once("open", () => {
     console.log("Database connected");
 });
@@ -34,20 +36,19 @@ db.once("open", () => {
 //get all poolfunds in the system
 app.get('/poolfunds', async (req, res) =>{
     const poolfunds = await PoolFund.find({});
-    res.render('pool_funds/index', {poolfunds});
+    res.status(200).json({})
+    // res.render('pool_funds/index', {poolfunds});
 })
 
-// Create new poolfund get page
 app.get('/poolfunds/new', (req, res) => {
-    // Retrieve enum values directly from the schema
     const paymentIntervals = PoolFund.schema.path('paymentInterval').enumValues; 
-    res.render('pool_funds/new', {paymentIntervals});
+    // res.render('pool_funds/new', {paymentIntervals});
 })
 
 //get specific poolfund information
 app.get('/poolfunds/:id', async (req, res) =>{
     const poolfund = await PoolFund.findById(req.params.id).populate('members');
-    res.render('pool_funds/show', {poolfund});
+    // res.render('pool_funds/show', {poolfund});
 })
 
 // create new poolfund post route
@@ -55,19 +56,19 @@ app.post('/poolfunds', async (req, res, next) => {
     const poolfund = new PoolFund(req.body.pool_fund);
     console.log(poolfund);
     await poolfund.save();
-    res.redirect(`/poolfunds/${poolfund._id}`)
+    // res.redirect(`/poolfunds/${poolfund._id}`)
 })
 
 //get all stokvel members
 app.get('/stokvelmembers', async (req, res) =>{
     const members = await User.find({});
-    res.send(members);
+    //res.send(members);
 })
 
 //get all transactions
 app.get('/transactions', async (req, res) =>{
     const transactions = await Transaction.find({});
-    res.send(transactions);
+    // res.send(transactions);
 })
 
 
@@ -75,11 +76,17 @@ app.get('/transactions', async (req, res) =>{
 //get specific poolfund information
 app.get('/users/:id', async (req, res) =>{
     const user = await User.findById(req.params.id).populate('pool_funds');
-    res.send(user);
+    // res.send(user);
 })
 
-//get transaction by member or pool fund (yet to come)
 
+app.get('/', async (req, res) =>{
+   try{
+    res.status(200).json({})
+   }catch(error){
+    res.status(500).json({})
+   }
+})
 
 app.listen(PORT, () => {
     console.log(`Serving on port: ${PORT}`)
